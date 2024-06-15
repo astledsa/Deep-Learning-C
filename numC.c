23361d26f74d18229bbe914e40d5c3257b706587
Astle Sylvester Dsa <astlesylvesterdsa@Astles-MacBook-Air.local>
2024-06-16 01:25:29 +0530

    Update matrix lib

--- numC.c
+++ numC.c
@@ -2,30 +2,30 @@
 // Important functionlities implemented
 
 // Matrix Creation
-// -- Zeros
-// -- Ones
-// -- Normal distribution
-// -- Identity
-// -- random
+// -- Zeros.
+// -- Ones.
+// -- Normal distribution.
+// -- Identity.
+// -- random.
 
 // Matrix Operations
-// -- Matrix Multiplication
-// -- Transpose
+// -- Matrix Multiplication.
+// -- Transpose.
 // -- Inverse
-// -- Trace
+// -- Trace.
 // -- Determinant
 
 // Element Wise Operations
-// -- Addition
-// -- Subtraction
+// -- Addition.
+// -- Subtraction.
 // -- Division
-// -- Multiplication
-// -- Log
-// -- Sin
-// -- Cos
-// -- Tan
-// -- Power
-// -- Square Root
+// -- Multiplication.
+// -- Log.
+// -- Sin.
+// -- Cos.
+// -- Tan.
+// -- Power.
+// -- Square Root.
 
 // Aggregation Functions
 // -- Summation
@@ -93,7 +93,7 @@ Matrix* CreateMatrix (int shape[2]) {
     assert (shape[0] > 0 && shape[1] > 0);
 
     Matrix* matrix = (Matrix*)malloc(sizeof(Matrix));
-    matrix->array = (double*)malloc(sizeof(shape[0] * shape[1]));
+    matrix->array = (double*)malloc((size_t)(shape[0] * shape[1]) * sizeof(double));
     matrix->shape[0] = shape[0];
     matrix->shape[1] = shape[1];
     matrix->stride[0] = shape[1];
@@ -119,7 +119,6 @@ Matrix* Ones (int shape[2]) {
 }
 
 Matrix* Random (int shape[2]) {
-    srand(time(NULL));
     Matrix* ones = CreateMatrix(shape);
     for (int i = 0; i < shape[0] * shape[1]; i++) {
         int rand_value = rand();
@@ -152,6 +151,204 @@ Matrix* Eye (int shape[2]) {
         }
     }
     return eye;
+}
+
+Matrix* ADD (Matrix* m1, Matrix* m2) {
+    assert (m1->shape[0] == m2->shape[0] && m1->shape[1] == m2->shape[1]);
+
+    Matrix* m3 = CreateMatrix(m1->shape);
+    for (int row = 0; row < m1->shape[0]; row++) {
+        for (int col = 0; col < m1->shape[1]; col++) {
+            m3->array[row * m3->stride[0] + col * m3->stride[1]] = m1->array[row * m1->stride[0] + col * m1->stride[1]]+
+                                                                   m2->array[row * m2->stride[0] + col * m2->stride[1]];
+        }
+    }
+    return m3;
+}
+
+Matrix* SUBTRACT (Matrix* m1, Matrix* m2) {
+    assert (m1->shape[0] == m2->shape[0] && m1->shape[1] == m2->shape[1]);
+
+    Matrix* m3 = CreateMatrix(m1->shape);
+    for (int row = 0; row < m1->shape[0]; row++) {
+        for (int col = 0; col < m1->shape[1]; col++) {
+            m3->array[row * m3->stride[0] + col * m3->stride[1]] = m1->array[row * m1->stride[0] + col * m1->stride[1]]-
+                                                                   m2->array[row * m2->stride[0] + col * m2->stride[1]];
+        }
+    }
+    return m3;
+}
+
+Matrix* MULT (Matrix* m1, Matrix* m2) {
+    assert (m1->shape[0] == m2->shape[0] && m1->shape[1] == m2->shape[1]);
+
+    Matrix* m3 = CreateMatrix(m1->shape);
+    for (int row = 0; row < m1->shape[0]; row++) {
+        for (int col = 0; col < m1->shape[1]; col++) {
+            m3->array[row * m3->stride[0] + col * m3->stride[1]] = m1->array[row * m1->stride[0] + col * m1->stride[1]]*
+                                                                   m2->array[row * m2->stride[0] + col * m2->stride[1]];
+        }
+    }
+    return m3;
+}
+
+Matrix* MATMUL (Matrix* m1, Matrix* m2) {
+    assert (m1->shape[1] == m2->shape[0]);
+
+    int new_shape[2] = {m1->shape[0], m2->shape[1]};
+    Matrix* m3 = CreateMatrix(new_shape);
+
+    for (int i = 0; i < m3->shape[0]; i++) {
+        for (int j=0; j < m3->shape[1]; j++) {
+            m3->array[i * m3->stride[0] + j * m3->stride[1]] = 0;
+            for (int k = 0; k < m1->shape[1]; k++) {
+                m3->array[i * m3->stride[0] + j * m3->stride[1]] += m1->array[i * m1->stride[0] + k * m1->stride[1]] *
+                                                                    m2->array[k * m2->stride[0] + j * m2->stride[1]];
+            }
+        }
+    }
+
+    return m3;
+}
+
+Matrix* Transpose (Matrix* m) {
+    int temp = m->shape[0];
+    int temp2 = m->stride[0];
+    m->shape[0] = m->shape[1];
+    m->stride[0] = m->stride[1];
+    m->stride[1] = temp2;
+    m->shape[1] = temp;
+
+    return m;
+}
+
+Matrix* SIN (Matrix* matrix) {
+    int rows = matrix->shape[0];
+    int cols = matrix->shape[1];
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            matrix->array[
+                (i * matrix->stride[0]) + 
+                (j * matrix->stride[1])
+            ] = sin(matrix->array[(i * matrix->stride[0]) + (j * matrix->stride[1])]);
+        }
+    }
+
+    return matrix;
+}
+
+Matrix* COS (Matrix* matrix) {
+    int rows = matrix->shape[0];
+    int cols = matrix->shape[1];
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            matrix->array[
+                (i * matrix->stride[0]) + 
+                (j * matrix->stride[1])
+            ] = cos(matrix->array[(i * matrix->stride[0]) + (j * matrix->stride[1])]);
+        }
+    }
+
+    return matrix;
+}
+
+Matrix* TAN (Matrix* matrix) {
+    int rows = matrix->shape[0];
+    int cols = matrix->shape[1];
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            assert (cos(matrix->array[
+                (i * matrix->stride[0]) + 
+                (j * matrix->stride[1])
+            ]) != 0);
+            matrix->array[
+                (i * matrix->stride[0]) + 
+                (j * matrix->stride[1])
+            ] = tan(matrix->array[(i * matrix->stride[0]) + (j * matrix->stride[1])]);
+        }
+    }
+
+    return matrix;
+}
+
+Matrix* LOG (Matrix* matrix) {
+    int rows = matrix->shape[0];
+    int cols = matrix->shape[1];
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            assert (matrix->array[
+                (i * matrix->stride[0]) + 
+                (j * matrix->stride[1])
+            ] != 0);
+            matrix->array[
+                (i * matrix->stride[0]) + 
+                (j * matrix->stride[1])
+            ] = log(matrix->array[(i * matrix->stride[0]) + (j * matrix->stride[1])]);
+        }
+    }
+
+    return matrix;
+}
+
+double Trace (Matrix* m) {
+    assert (m->shape[0] == m->shape[1]);
+
+    double sum = 0;
+    for (int i = 0; i < m->shape[0]; i++) {
+        for (int j = 0; j < m->shape[1]; j++) {
+            if (i == j) {
+                sum += m->array[(i * m->stride[0]) + (j * m->stride[1])]; 
+            }
+        }
+    }
+    return sum;
+}
+
+Matrix* ELEMENT_POW (Matrix* matrix, double n) {
+    int rows = matrix->shape[0];
+    int cols = matrix->shape[1];
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            matrix->array[
+                (i * matrix->stride[0]) + 
+                (j * matrix->stride[1])
+            ] = pow(matrix->array[(i * matrix->stride[0]) + (j * matrix->stride[1])], n);
+        }
+    }
+
+    return matrix;
+}
+
+Matrix* SQRT (Matrix* m) {
+    return ELEMENT_POW(m, 0.5);
+}
+
+Matrix* POW (Matrix* matrix, int n) {
+    Matrix* res = Ones(matrix->shape);
+    while (n > 0) {
+        res = MATMUL(res, matrix);
+        n --;
+    }
+    return res;
+}
+
+
+
+int main() {
+    // srand(time(NULL));
+
+    // int shape[2] = {5, 5};
+
+    // Matrix* I = Random(shape);
+
+    // Print(I);
+    // printf("\n");
+    // Print(LOG(I));
 }
 
 // gcc numC.c -o exec
